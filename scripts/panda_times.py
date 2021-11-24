22864f6ac0790659db13cff562387d546dd9d475
gpollayil <gpollayil@gmail.com>
2021-11-24 15:55:43 +0100

    Fixed minor bugs. Now starting to save success and times

--- scripts/panda_times.py
+++ scripts/panda_times.py
@@ -9,7 +9,6 @@ import rospy
 import moveit_commander
 import moveit_msgs.msg
 import geometry_msgs.msg
-from math import pi
 from std_msgs.msg import String
 from moveit_commander.conversions import pose_to_list
 
@@ -23,43 +22,51 @@ pose_goal.position.z = 0.4
 
 def main_fun():
 
-    # Initialize moveit_commander and a rospy node
-    moveit_commander.roscpp_initialize(sys.argv)
-    rospy.init_node('panda_times', anonymous=True)
+    try:
 
-    print('Built commander!')
+        # Initialize moveit_commander and a rospy node
+        moveit_commander.roscpp_initialize(sys.argv)
+        rospy.init_node('panda_times', anonymous=True)
 
-    # This gives robot kinematic model and the robot current joint states
-    robot = moveit_commander.RobotCommander()
+        print('Built commander!')
 
-    # This provides a remote interface for getting, setting, and updating the 
-    # robots internal understanding of the surrounding world
-    scene = moveit_commander.PlanningSceneInterface()
+        # This gives robot kinematic model and the robot current joint states
+        robot = moveit_commander.RobotCommander()
 
-    # This object is an interface to a planning group (group of joints)
-    group_name = "panda_arm"
-    move_group = moveit_commander.MoveGroupCommander(group_name)
+        # This provides a remote interface for getting, setting, and updating the 
+        # robots internal understanding of the surrounding world
+        #scene = moveit_commander.PlanningSceneInterface()
+        moveit_commander.PlanningSceneInterface()
 
-    # To display trajectories in Rviz
-    display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path', 
-        moveit_msgs.msg.DisplayTrajectory, queue_size=20)
+        # This object is an interface to a planning group (group of joints)
+        group_name = "panda_arm"
+        move_group = moveit_commander.MoveGroupCommander(group_name)
 
-    # Printout info 
-    if print_details:
-        display_info(move_group, robot) 
+        # To display trajectories in Rviz
+        display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path', 
+            moveit_msgs.msg.DisplayTrajectory, queue_size=20)
 
-    # Plan to pose and display trajectory
-    plan_traj_pose = plan_pose(move_group, pose_goal) 
-    display_trajectory(robot, plan_traj_pose, display_trajectory_publisher)
+        # Printout info 
+        if print_details:
+            display_info_fun(move_group, robot) 
 
-    # Execute trajectory
-    move_group.execute(plan_traj_pose, wait=True)
+        # Change planning algorithm
+        move_group.set_planner_id("RRTkConfigDefault")
+        print("The set planner is " + move_group.get_planner_id())
 
-    print("Finished the node.")
+        # Plan and go to pose
+        plan_traj_pose = plan_pose_fun(move_group, pose_goal) 
+
+        print("Finished the node.")
+
+    except rospy.ROSInterruptException:
+        return
+    except KeyboardInterrupt:
+        return
 
 
 # Function for displaying info about move group and robot
-def display_info(move_group, robot):
+def display_info_fun(move_group, robot):
     # Get the name of the reference frame for this robot
     planning_frame = move_group.get_planning_frame()
     print "============ Planning frame: %s" % planning_frame
@@ -79,7 +86,7 @@ def display_info(move_group, robot):
 
 
 # Function for planing to a pose
-def plan_pose(move_group, pose_goal):
+def plan_pose_fun(move_group, pose_goal):
 
     move_group.set_pose_target(pose_goal)
     plan = move_group.go(wait=True)
@@ -89,13 +96,6 @@ def plan_pose(move_group, pose_goal):
     move_group.clear_pose_targets()
 
     return plan
-
-# Function for displaying a planned trajectory
-def display_trajectory(robot, plan, display_trajectory_publisher):
-    display_trajectory = moveit_msgs.msg.DisplayTrajectory()
-    display_trajectory.trajectory_start = robot.get_current_state()
-    display_trajectory.trajectory.append(plan)
-    display_trajectory_publisher.publish(display_trajectory)
 
 
 # MAIN
